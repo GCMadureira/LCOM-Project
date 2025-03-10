4e1c5980d366da9339bf7bcb82bcdf9918f135e2
Gustavo Madureira <gcmadureira2017@gmail.com>
2025-03-10 20:32:56 +0000

    Added timer_set_frequency function + code cleanup

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,10 +6,38 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint8_t status;
+  if(timer_get_conf(timer, &status) != 0) return 1; //get config as not to change the first 4 bits
+  status &= 0x0F;
 
-  return 1;
+  if(freq < 19) return 1; //prevent overflow
+  uint16_t realFreq = TIMER_FREQ/freq;
+  uint8_t LSB; util_get_LSB(realFreq, &LSB);
+  uint8_t MSB; util_get_MSB(realFreq, &MSB);
+
+  switch (timer){
+    case 0:
+    case TIMER_0:
+      sys_outb(TIMER_CTRL, 0x30 | status); //0b0011xxxx
+      sys_outb(TIMER_0, LSB);
+      sys_outb(TIMER_0, MSB);
+      break;
+    case 1:
+    case TIMER_1:
+      sys_outb(TIMER_CTRL, 0x60 | status); //0b0111xxxx
+      sys_outb(TIMER_1, LSB);
+      sys_outb(TIMER_1, MSB);
+      break;
+    case 2:
+    case TIMER_2:
+      sys_outb(TIMER_CTRL, 0xB0 | status); //0b1011xxxx
+      sys_outb(TIMER_2, LSB);
+      sys_outb(TIMER_2, MSB);
+      break;
+    default: //invalid timer
+      return 1;
+  }
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -33,12 +61,20 @@ void (timer_int_handler)() {
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   switch (timer){
+    case 0:
     case TIMER_0:
       sys_outb(TIMER_CTRL, 0xC2); //0b11000010
+      break;
+    case 1:
     case TIMER_1:
       sys_outb(TIMER_CTRL, 0xC4); //0b11000100
+      break;
+    case 2:
     case TIMER_2:
       sys_outb(TIMER_CTRL, 0xC8); //0b11001000
+      break;
+    default: //invalid timer
+      return 1;
   }
   return util_sys_inb(timer, st);
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -1,5 +1,4 @@
 #include <lcom/lcf.h>
-
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
@@ -13,16 +12,10 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 }
 
 int util_sys_inb(int port, uint8_t *value) {
-    uint32_t temp_value; // Temporary variable to hold 32 bit variable
-
-    // Call sys_inb to read the 32 bit
+    uint32_t temp_value;
     if (sys_inb(port, &temp_value) != 0) {
-        return 1; // If sys_inb != 0 it means an error occurred so return 1
+        return 1;
     }
-
-    // Copy the LSB of the 32 bit variable to the desired 8 bit variable
     *value = (uint8_t)(temp_value & 0xFF);
-
-
     return 0;
 }
