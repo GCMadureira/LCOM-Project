e890124eaa857fc8380525ce63557b5f2d53ded7
GustavoCMadureira <up202304978@up.pt>
2025-04-05 21:19:02 +0100

    Added lab4's mouse_test_packet test function

--- lab4/keyboard.h
+++ lab4/keyboard.h
@@ -4,7 +4,9 @@
 #include <lcom/lcf.h>
 #include <lcom/utils.h>
 #include <lcom/timer.h>
+
 #include "i8042.h"
+#include "kbc.h"
 
 int (keyboard_subscribe_int_exclusive)(uint8_t *bit_no);
 int (keyboard_unsubscribe_int)();
@@ -13,9 +15,6 @@ bool (is_breakcode)(uint8_t scancode);
 u_int8_t (get_scancode)();
 uint32_t (get_sys_inb_calls)();
 
-int (write_kbc_command)(uint8_t command);
-int (write_kbc_command_arg)(uint8_t command, uint8_t arg);
-int (read_kbc_command)(uint8_t* command);
 int (keyboard_reenable_interrupts)();
 
 
--- lab4/lab4.c
+++ lab4/lab4.c
@@ -6,17 +6,20 @@
 
 // Any header files included below this line should have been created by you
 
+#include "kbc.h"
+#include "mouse.h"
+
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
   lcf_set_language("EN-US");
 
   // enables to log function invocations that are being "wrapped" by LCF
   // [comment this out if you don't want/need/ it]
-  lcf_trace_calls("/home/lcom/labs/lab4/trace.txt");
+  //lcf_trace_calls("/home/lcom/labs/lab4/trace.txt");
 
   // enables to save the output of printf function calls on a file
   // [comment this out if you don't want/need it]
-  lcf_log_output("/home/lcom/labs/lab4/output.txt");
+  //lcf_log_output("/home/lcom/labs/lab4/output.txt");
 
   // handles control over to LCF
   // [LCF handles command line arguments and invokes the right function]
@@ -32,9 +35,57 @@ int main(int argc, char *argv[]) {
 
 
 int (mouse_test_packet)(uint32_t cnt) {
-    /* To be completed */
-    printf("%s(%u): under construction\n", __func__, cnt);
-    return 1;
+  int ipc_status, r;
+  uint8_t bit_no = 0;
+  message msg;
+
+  uint8_t packet_bytes[3];
+  int state = 0; //0 means first byte, 1 means second by, 2 means third byte
+ 
+  if(mouse_subscribe_int_exclusive(&bit_no)) return 1;
+  if(mouse_stream_enable_data_reporting()) return 1;
+
+  while(cnt > 0) {
+    /* Get a request message. */
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE: /* hardware interrupt notification */				
+          if (msg.m_notify.interrupts & BIT(bit_no)) { /* subscribed interrupt */
+            if(!valid_kbc_output(true)){ //invalid output on kbc's output register
+              discard_kbc_output();
+              continue;
+            }
+
+            mouse_ih();
+            uint8_t packet_byte = get_mouse_packet_byte();
+            --cnt;
+
+            // the first byte always has the bit 3 on, skip byte if it does not for synchronization
+            if(state == 0 && !(packet_byte & BIT(3))) continue;
+            packet_bytes[state] = packet_byte;
+            if(state == 2){
+              struct packet packet = assemble_packet(packet_bytes);
+              mouse_print_packet(&packet);
+            }
+            state = (state + 1) % 3;
+          }
+          break;
+        default:
+          break; /* no other notifications expected: do nothing */	
+      }
+    } else { /* received a standard message, not a notification */
+      /* no standard messages expected: do nothing */
+    }
+  }
+
+  if(mouse_unsubscribe_int()) return 1;
+  if(mouse_stream_disable_data_reporting()) return 1;
+
+  return 0;
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
@@ -43,7 +94,7 @@ int (mouse_test_async)(uint8_t idle_time) {
     return 1;
 }
 
-int (mouse_test_gesture)() {
+int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
     /* To be completed */
     printf("%s: under construction\n", __func__);
     return 1;
