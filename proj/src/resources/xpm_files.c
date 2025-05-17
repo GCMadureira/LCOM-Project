2bbaf4fe37baf3503ed5f9453992d7701225dc66
Tiago Mota Cunha <up202305564@up.pt>
2025-05-17 10:04:12 +0100

    Refactored each XPM to its own file

--- proj/src/resources/xpm_files.c
+++ proj/src/resources/xpm_files.c
@@ -1,7 +1,14 @@
 #include "xpm_files.h"
 
+//  XPMs
+#include "xpms/background.xpm"
+#include "xpms/pharaoh.xpm"
+#include "xpms/start_selected.xpm"
+#include "xpms/quit_selected.xpm"
+#include "xpms/game_background.xpm"
+
+
 int (loadAllXpm)() {
-  #include "xpm_files.txt"
 
   if(xpm_load(sprite, XPM_8_8_8_8, &sprite_img) == NULL) return 1;
   if(xpm_load(game_background_xpm, XPM_8_8_8_8, &game_background_img) == NULL) return 1;
